f633c6d8adcc1cf497dc5e4288a209bee5169c34
Takeshi Nakamura <taqueci.n@gmail.com>
2019-02-22 19:14:21 +0900

    Support Redmine 4

--- app/controllers/redmine_extended_reminder/settings_controller.rb
+++ app/controllers/redmine_extended_reminder/settings_controller.rb
@@ -1,5 +1,5 @@
 class RedmineExtendedReminder::SettingsController < ApplicationController
-  before_filter :require_login
+  before_action :require_login
 
   def update
     user = User.current
--- lib/redmine_extended_reminder/mailer_model_patch.rb
+++ lib/redmine_extended_reminder/mailer_model_patch.rb
@@ -3,24 +3,20 @@ require_dependency 'mailer'
 module RedmineExtendedReminder
   module MailerModelPatch
     def self.included(base)
-      base.extend(ClassMethods)
-      base.send(:include, InstanceMethods)
+      class << base
+        self.prepend(ClassMethods)
+      end
+      base.send(:prepend, InstanceMethods)
 
       base.class_eval do
         # replace class methods
         helper :extended_reminder
-        class << self
-          alias_method_chain :reminders, :patch
-        end
-
-        # replace instance methods
-        alias_method_chain :reminder, :patch
       end
     end
   end
 
   module ClassMethods
-    def reminders_with_patch(options={})
+    def reminders(options={})
       # Non-working days setting is supported in Redmine 2.2 or higher.
       if Redmine::VERSION.to_a[0..1].join('.').to_f >= 2.2
         if Setting.plugin_redmine_extended_reminder[:disable_on_non_woking_days].to_i != 0
@@ -32,19 +28,19 @@ module RedmineExtendedReminder
 
       days_override = Setting.plugin_redmine_extended_reminder[:days].to_i
       options[:days] = days_override if days_override > 0
-      reminders_without_patch(options)
+      super(options)
     end
   end
 
   module InstanceMethods
-    def reminder_with_patch(user, issues, days)
+    def reminder(user, issues, days)
       saved_status = ActionMailer::Base.perform_deliveries
       if user.pref[:extended_reminder_no_reminders]
         ActionMailer::Base.perform_deliveries = false
       end
       @issues_by_date = issues.sort_by(&:due_date).group_by(&:due_date)
       @count = issues.size
-      reminder_without_patch(user, issues, days)
+      super(user, issues, days)
     ensure
       ActionMailer::Base.perform_deliveries = saved_status
     end
